157144a9a23db1a4f962d2522939014c60a75ecc
Jacob Von Ogden <jvon1197@gmail.com>
2021-11-22 20:58:06 -0500

    preparing for heroku deploy

--- red-badge-server/db.js
+++ red-badge-server/db.js
@@ -2,6 +2,12 @@ const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-})
+  ssl: process.env.ENVIRONMENT === 'production',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
 
 module.exports = db;
